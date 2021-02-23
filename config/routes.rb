b955541092c11a05cb3bbd1e1205c57fc6e4379d
Garllon <palluthe.bennet@gmail.com>
2021-02-23 21:27:23 +0100

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'bottle_deposit_storages#index'
+  root to: 'bottle_deposit/configurations#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :bottle_deposit do
     resources :configurations, except: [:show]
