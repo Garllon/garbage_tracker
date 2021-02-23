eaf77789cd6e56bcf5ecf0dad3c556ee4a6a4fc8
Garllon <palluthe.bennet@gmail.com>
2021-02-23 21:31:34 +0100

    rubocop -a

--- db/schema.rb
+++ db/schema.rb
@@ -10,38 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_173015) do
-
+ActiveRecord::Schema.define(version: 20_210_217_173_015) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "bottle_deposit_configurations", force: :cascade do |t|
-    t.string "name"
-    t.decimal "price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'bottle_deposit_configurations', force: :cascade do |t|
+    t.string 'name'
+    t.decimal 'price'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "bottle_deposit_storages", force: :cascade do |t|
-    t.string "name"
-    t.integer "amount"
-    t.bigint "bottle_deposit_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["bottle_deposit_id"], name: "index_bottle_deposit_storages_on_bottle_deposit_id"
+  create_table 'bottle_deposit_storages', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'amount'
+    t.bigint 'bottle_deposit_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['bottle_deposit_id'], name: 'index_bottle_deposit_storages_on_bottle_deposit_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  add_foreign_key "bottle_deposit_storages", "bottle_deposit_configurations", column: "bottle_deposit_id"
+  add_foreign_key 'bottle_deposit_storages', 'bottle_deposit_configurations', column: 'bottle_deposit_id'
 end
