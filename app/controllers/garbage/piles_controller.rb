7beade425e16f0ac7fe718ebd339e4e53bf2ffa2
Garllon <palluthe.bennet@gmail.com>
2021-03-06 23:18:27 +0100

    rename type and fix associations and add new and edit

--- app/controllers/garbage/piles_controller.rb
+++ app/controllers/garbage/piles_controller.rb
@@ -7,5 +7,27 @@ module Garbage
     def index
       @piles = Pile.includes(:container).where(user_id: current_user.id)
     end
+
+    def new
+      @pile = Pile.new
+    end
+
+    def create
+      byebug
+      Pile.create!(pile_params)
+      redirect_to garbage_piles_path
+    end
+
+    private
+
+    def pile_params
+      pile_params = params.require(:garbage_pile)
+                          .permit(:weight,
+                                  :kind,
+                                  :container_id,
+                                  :produced_at)
+      pile_params[:user_id] = current_user.id
+      pile_params
+    end
   end
 end
--- db/migrate/20210305204734_add_type_to_pills.rb
+++
@@ -1,5 +0,0 @@
-class AddTypeToPills < ActiveRecord::Migration[6.1]
-  def change
-    add_column :garbage_piles, :type, :integer
-  end
-end
