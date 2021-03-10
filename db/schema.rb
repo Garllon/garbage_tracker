5d0a8c3e5377d2d04862a37b86128477e5cc3986
Garllon <palluthe.bennet@gmail.com>
2021-03-10 09:42:14 +0100

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,59 +10,60 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_307_204_511) do
+ActiveRecord::Schema.define(version: 2021_03_07_204511) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'bottle_deposit_configurations', force: :cascade do |t|
-    t.string 'name'
-    t.decimal 'price'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "bottle_deposit_configurations", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'bottle_deposit_storages', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'amount'
-    t.bigint 'bottle_deposit_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['bottle_deposit_id'], name: 'index_bottle_deposit_storages_on_bottle_deposit_id'
+  create_table "bottle_deposit_storages", force: :cascade do |t|
+    t.string "name"
+    t.integer "amount"
+    t.bigint "bottle_deposit_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["bottle_deposit_id"], name: "index_bottle_deposit_storages_on_bottle_deposit_id"
   end
 
-  create_table 'garbage_containers', force: :cascade do |t|
-    t.string 'name'
-    t.decimal 'weight'
-    t.bigint 'user_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['user_id'], name: 'index_garbage_containers_on_user_id'
+  create_table "garbage_containers", force: :cascade do |t|
+    t.string "name"
+    t.decimal "weight"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_garbage_containers_on_user_id"
   end
 
-  create_table 'garbage_piles', force: :cascade do |t|
-    t.decimal 'weight'
-    t.date 'produced_at'
-    t.bigint 'container_id'
-    t.bigint 'user_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.integer 'kind'
-    t.integer 'real_weight'
-    t.index ['container_id'], name: 'index_garbage_piles_on_container_id'
-    t.index ['user_id'], name: 'index_garbage_piles_on_user_id'
+  create_table "garbage_piles", force: :cascade do |t|
+    t.decimal "weight"
+    t.date "produced_at"
+    t.bigint "container_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "kind"
+    t.integer "real_weight"
+    t.index ["container_id"], name: "index_garbage_piles_on_container_id"
+    t.index ["user_id"], name: "index_garbage_piles_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'bottle_deposit_storages', 'bottle_deposit_configurations', column: 'bottle_deposit_id'
+  add_foreign_key "bottle_deposit_storages", "bottle_deposit_configurations", column: "bottle_deposit_id"
 end
