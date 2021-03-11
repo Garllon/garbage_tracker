804f0967cb0e1d48cd271e78a16c1c25c9505010
Garllon <palluthe.bennet@gmail.com>
2021-03-11 13:30:01 +0100

    rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,43 +12,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_204511) do
-
+ActiveRecord::Schema.define(version: 20_210_307_204_511) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "containers", force: :cascade do |t|
-    t.string "name"
-    t.decimal "weight"
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_containers_on_user_id"
+  create_table 'containers', force: :cascade do |t|
+    t.string 'name'
+    t.decimal 'weight'
+    t.bigint 'user_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['user_id'], name: 'index_containers_on_user_id'
   end
 
-  create_table "piles", force: :cascade do |t|
-    t.decimal "weight"
-    t.date "produced_at"
-    t.bigint "container_id"
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "kind"
-    t.integer "real_weight"
-    t.index ["container_id"], name: "index_piles_on_container_id"
-    t.index ["user_id"], name: "index_piles_on_user_id"
+  create_table 'piles', force: :cascade do |t|
+    t.decimal 'weight'
+    t.date 'produced_at'
+    t.bigint 'container_id'
+    t.bigint 'user_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.integer 'kind'
+    t.integer 'real_weight'
+    t.index ['container_id'], name: 'index_piles_on_container_id'
+    t.index ['user_id'], name: 'index_piles_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
-
 end
