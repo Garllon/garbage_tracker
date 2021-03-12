c465fbc7f324c139f9cd5c68528281e0612c96f7
Garllon <palluthe.bennet@gmail.com>
2021-03-12 11:47:03 +0100

    add row olor for different waste kind

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,17 @@ module ApplicationHelper
     'alert' => 'alert-danger'
   }.freeze
 
+  WASTE_BACKGROUND_COLOR = {
+    'paper' => 'primary',
+    'waste' => 'dark',
+    'plastic' => 'warning'
+  }.freeze
+
   def flash_class(level)
     "alert #{FLASH_LEVEL[level]}"
+  end
+
+  def row_background_color(waste_kind)
+    "table-#{WASTE_BACKGROUND_COLOR[waste_kind]}"
   end
 end
