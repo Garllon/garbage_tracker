e682936d36d1a8460905ea79fad0a7b1459fd963
Garllon <palluthe.bennet@gmail.com>
2021-03-16 19:06:04 +0100

    add beta plot idea

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,6 +1,43 @@
+# frozen_string_literal: true
+
 class GraphsController < ApplicationController
   def index
-    @matrix = Pile.kinds.keys.each_with_object({}) do |kind, hash|
+    chart
+
+    @chart_globals = LazyHighCharts::HighChartGlobals.new do |f|
+      f.global(useUTC: false)
+      f.colors(['#ae5904', '#3B5998', '#fbe400', '#0b0b0b'])
+    end
+  end
+
+  private
+
+  def chart
+    @chart ||= LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(text: 'Your garbage of the last month')
+      f.xAxis(categories: matrix['bio'][:month])
+      matrix.each do |key, value|
+        f.series(name: key, yAxis: 0, data: value[:data])
+      end
+
+      f.yAxis({
+                title: { text: 'Garbage in Gramm', margin: 70 },
+                # TODO: Plotlines is maybe not the best here. Needs optimisation
+                plotLines: [{
+                  color: 'yellow',
+                  width: 2,
+                  value: 9500,
+                  zIndex: 5
+                }]
+              })
+
+      f.legend(align: 'right', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical')
+      f.chart({ defaultSeriesType: 'column' })
+    end
+  end
+
+  def matrix
+    @matrix ||= Pile.kinds.keys.each_with_object({}) do |kind, hash|
       kind_data = Pile.where(user_id: current_user.id)
                       .where(kind: kind)
                       .group("DATE_TRUNC('month', produced_at)")
@@ -8,26 +45,6 @@ class GraphsController < ApplicationController
 
       hash[kind] = { data: kind_data.values,
                      month: kind_data.keys.map(&:month) }
-    end
-
-    @chart = LazyHighCharts::HighChart.new('graph') do |f|
-      f.title(text: 'Your garbage of the last month')
-      f.xAxis(categories: @matrix['bio'][:month])
-      @matrix.each do |key, value|
-        f.series(name: key, yAxis: 0, data: value[:data])
-      end
-
-      f.yAxis [
-        {title: {text: "Garbage in Gramm", margin: 70} },
-      ]
-
-      f.legend(align: 'right', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical')
-      f.chart({defaultSeriesType: "column"})
-    end
-
-    @chart_globals = LazyHighCharts::HighChartGlobals.new do |f|
-      f.global(useUTC: false)
-      f.colors(["#ae5904", "#3B5998", "#fbe400", "#0b0b0b"])
     end
   end
 end
