4dc4b691a737e57b1140085193adf6d651205179
Garllon <palluthe.bennet@gmail.com>
2021-04-18 21:26:00 +0200

    add missing authenticaation_check method

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class GraphsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @graph_presenter = GraphPresenter.new(current_user.id)
   end
